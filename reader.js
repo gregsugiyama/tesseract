8f179370102ca28ae85a417922a8c8062bc2182c
Greg Sugiyama <greg.sugiyama@bench.co>
2022-06-30 18:16:27 -0700

    Adds reader and test data

---
+++ reader.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const { createWorker } = require("tesseract.js");
+
+const [, , imagePath] = process.argv;
+const image = path.resolve(__dirname, imagePath);
+
+console.log(`Processing ${image}`);
+
+const worker = createWorker({
+  logger: (m) => console.log(m),
+});
+
+(async () => {
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+  const {
+    data: { text },
+  } = await worker.recognize(image);
+  console.log(text);
+  await worker.terminate();
+})();
